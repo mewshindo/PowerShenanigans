3993a57f3893699a1e294cb161f8b607e60937fa
mewshindo <hellrayz11@gmail.com>
2025-10-15 20:17:20 +0300

    timerbase

---
+++ DebugLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerShenanigans
+{
+    public static class DebugLogger
+    {
+        public static void Log(string message)
+        {
+            if (Plugin.Instance.DevMode)
+            {
+                Console.WriteLine($"[PowerShenanigans] {message}");
+            }
+        }
+        public static void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[PowerShenanigans][ERROR] {message}");
+            Console.ResetColor();
+        }
+        public static void LogWarning(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[PowerShenanigans][WARNING] {message}");
+            Console.ResetColor();
+        }
+    }
+}
